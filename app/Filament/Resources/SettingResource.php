0e49dd2bdc46b819c97c4de0ff676c68b473321f
leveluperu-dev <levelupnegocio@gmail.com>
2026-02-01 01:55:06 -0500

    Feat: Implement reCAPTCHA security and Admin Settings management

---
+++ app/Filament/Resources/SettingResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SettingResource\Pages;
+use App\Models\Setting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class SettingResource extends Resource
+{
+    protected static ?string $model = Setting::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+
+    protected static ?string $navigationGroup = 'System';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('key')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+                Forms\Components\Textarea::make('value')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('key')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('value')->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSettings::route('/'),
+        ];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,21 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->isProduction()) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+
+        try {
+            if (\Illuminate\Support\Facades\Schema::hasTable('settings')) {
+                $settings = \App\Models\Setting::all();
+                foreach ($settings as $setting) {
+                    if ($setting->key === 'RECAPTCHA_SITE_KEY') {
+                        config(['recaptcha.api_site_key' => $setting->value]);
+                    }
+                    if ($setting->key === 'RECAPTCHA_SECRET_KEY') {
+                        config(['recaptcha.api_secret_key' => $setting->value]);
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            // connection might not be ready during migration
+        }
     }
 }
