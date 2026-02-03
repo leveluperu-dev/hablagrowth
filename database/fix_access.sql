a1bc05ff0081ea97fce0074409c8f8d3e226187d
leveluperu-dev <levelupnegocio@gmail.com>
2026-02-03 00:18:10 -0500

    Fix: Force reset of DB user credentials

--- database/fix_access.sql
+++ database/fix_access.sql
@@ -1,5 +1,9 @@
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hablagrowth_user'@'%' IDENTIFIED BY 'hablagrowth_password';
+-- Ensure database exists
+CREATE DATABASE IF NOT EXISTS hablagrowth;
+
+-- Reset the user completely to ensure password matches
+DROP USER IF EXISTS 'hablagrowth_user'@'%';
+CREATE USER 'hablagrowth_user'@'%' IDENTIFIED BY 'hablagrowth_password';
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON hablagrowth.* TO 'hablagrowth_user'@'%';
