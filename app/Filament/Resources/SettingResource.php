cfb58f6fa44ef8a2f66fa34b9da43e20ba30eafe
leveluperu-dev <levelupnegocio@gmail.com>
2026-02-02 23:41:10 -0500

    Fix: Revert strict types for Filament 3 compatibility

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -14,9 +14,9 @@ class SettingResource extends Resource
 {
     protected static ?string $model = Setting::class;
 
-    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-cog';
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
 
-    protected static string|\UnitEnum|null $navigationGroup = 'System';
+    protected static ?string $navigationGroup = 'System';
 
     public static function form(Form $form): Form
     {
